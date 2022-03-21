975f6447c4568f37226ccaba0d9b0b05787bbb8e
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-03-21 11:19:41 -0700

    Render orderbook with bars

--- client/src/components/OrderBook.js
+++ client/src/components/OrderBook.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./OrderBook.css";
 
 const OrderBook = ({ symbol }) => {
-	const [orders, setOrders] = useState([]);
+	const [orders, setOrders] = useState({ bids: [], asks: [] });
 	const currencyPair = `${symbol}usd`;
 
 	const currencyArray = currencyPair.toUpperCase().match(/.{1,3}/g);
@@ -21,7 +21,9 @@ const OrderBook = ({ symbol }) => {
 		};
 		ws.onmessage = (event) => {
 			const response = JSON.parse(event.data);
-			setOrders(response.data);
+			if (response.data.bids && response.data.asks) {
+				setOrders(response.data);
+			}
 		};
 		ws.onclose = () => {
 			ws.close();
@@ -33,11 +35,12 @@ const OrderBook = ({ symbol }) => {
 	}, [currencyPair]);
 
 	const { bids, asks } = orders;
-	const minOfBidsAndAsks = Math.min(bids?.length, asks?.length, 25);
+
+	const minOfBidsAndAsks = Math.min(bids.length, asks.length, 25);
 
 	function addTotal(bidsOrAsks) {
 		let total = 0;
-		return bidsOrAsks?.map((bidsOrAsks, index) => {
+		return bidsOrAsks.map((bidsOrAsks, index) => {
 			total += parseFloat(bidsOrAsks[1]);
 
 			return {
@@ -49,45 +52,59 @@ const OrderBook = ({ symbol }) => {
 		});
 	}
 
+	// adding total of bids and asks
+
 	const bidsWithTotal = addTotal(bids);
 	const asksWithTotal = addTotal(asks);
 
-	function addPercent(tot1, tot2) {
-		return parseFloat(tot1) / (parseFloat(tot1) + parseFloat(tot2));
-	}
+	const bidsAndAsksTotal =
+		bidsWithTotal[24]?.total || 0 + asksWithTotal[24]?.total || 0;
 
+	// adding percent
 	for (let i = 0; i < minOfBidsAndAsks; i++) {
-		bidsWithTotal[i].percent = addPercent(
-			bidsWithTotal[i].total,
-			asksWithTotal[i].total
-		);
-		asksWithTotal[i].percent = addPercent(
-			asksWithTotal[i].total,
-			bidsWithTotal[i].total
-		);
+		bidsWithTotal[i].percent = bidsWithTotal[i].total / bidsAndAsksTotal;
+		asksWithTotal[i].percent = asksWithTotal[i].total / bidsAndAsksTotal;
 	}
 
 	const orderRows = (arr, type) =>
 		arr &&
-		arr.map(
-			(item, index) =>
-				// have to limit asks and bids to min because sometimes it's not even and the cells arent aligned
+		arr.map((item, index) => {
+			console.log(`This is item.percent`);
+			console.log(item.percent * 100, index);
+
+			// have to limit asks and bids to min because sometimes it's not even and the cells arent aligned
+			return (
 				index < minOfBidsAndAsks && (
 					<tr
 						className={`OrderBook_row OrderBook_row_${type}`}
 						key={item.id}
 						style={{
 							background: `rgb(255,255,255)`,
-							background: `linear-gradient(90deg, rgba(255,255,255,0) ${
-								item.percent * 100
-							}%, #0064024f  ${item.percent * 100}%)`,
+							background: `linear-gradient(90deg, rgba(200,${
+								type === "bids" ? 255 : 30
+							},${type === "bids" ? 255 : 30},${
+								type === "bids" ? 0 : 0.3
+							}) ${
+								(type === "bids"
+									? 1 - item.percent
+									: item.percent) * 100
+							}%, rgba(${type === "bids" ? 30 : 255},${
+								type === "bids" ? 200 : 255
+							},${type === "bids" ? 30 : 255},${
+								type === "bids" ? 0.3 : 0
+							})  ${
+								(type === "bids"
+									? 1 - item.percent
+									: item.percent) * 100
+							}%)`,
 						}}
 					>
 						<td> {item.size} </td>
 						<td> {item.price} </td>
 					</tr>
 				)
-		);
+			);
+		});
 	const orderHead = (title) => (
 		<thead>
 			<tr>
