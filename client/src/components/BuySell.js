57652267442d3e5d9c911c57bd05155c17675df2
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-03-22 17:32:47 -0700

    Fix entry price imput

--- client/src/components/BuySell.js
+++ client/src/components/BuySell.js
@@ -1,59 +1,104 @@
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
 import "./BuySell.css";
+import { Typography, Button, Container, Input, TextField } from "@mui/material";
+import { styled, createTheme } from "@mui/material/styles";
+
+const MyTextField = styled(TextField)({
+    width:'50%',
+    marginBottom:20,
+	"& label.Mui-focused": {
+		color: "primary",
+	},
+	"& label": {
+		color: "lightgrey",
+	},
+	"& .MuiInput-underline:after": {
+		borderBottomColor: "transparent",
+	},
+	"& .MuiInput-root": {
+		borderBottom: "2px solid grey",
+		"&:hover ": {
+			borderBottom: "2px solid lightgrey",
+		},
+	},
+});
 
 function BuySell() {
 	const [entryPrice, setEntryPrice] = useState("");
 	const [type, setType] = useState("");
+	const [entryPriceError, setEntryPriceError] = useState(false);
 	const [buySellSelector, setBuySellSelector] = useState("buy");
 
 	function handleSubmit(e) {
 		e.preventDefault();
 		console.log(`submitted`);
+		if (entryPrice === "") {
+			setEntryPriceError(true);
+		} else {
+			setEntryPriceError(false);
+		}
 	}
 	return (
-		<form>
-			<div className="BuySell_selector_container">
-				<Button
-					className={`BuySell_selector ${
-						buySellSelector === "buy" && "buy"
-					}`}
-					onClick={() => setBuySellSelector("buy")}
+		<Container>
+			<form noValidate autoComplete="off">
+				<div className="BuySell_selector_container">
+					<Button
+						// variant means what it will look like, component means what html tag will be
+						variant="text"
+						className={`BuySell_selector ${
+							buySellSelector === "buy" && "buy"
+						}`}
+						onClick={() => setBuySellSelector("buy")}
+					>
+						Buy
+					</Button>
+					<Button
+						variant="text"
+						className={`BuySell_selector ${
+							buySellSelector === "sell" && "sell"
+						}`}
+						onClick={() => setBuySellSelector("sell")}
+					>
+						Sell
+					</Button>
+				</div>
+
+				<MyTextField
+					required
+					variant="standard"
+					label="Entry price"
+					type="number"
+					id="entryPrice"
+					name="entryPrice"
+					value={entryPrice}
+					onChange={(e) => setEntryPrice(e.target.value)}
+					error={entryPriceError}
+					className="BuySell_entryPrice"
+					InputLabelProps={{
+						shrink: true,
+					}}
+				/>
+
+				<label htmlFor="type">Type</label>
+				<select
+					name="type"
+					id="type"
+					value={type}
+					onChange={(e) => setType(e.target.value)}
 				>
-					Buy
-				</Button>
+					<option value="market">Market</option>
+					<option value="limit">Limit</option>
+				</select>
 				<Button
-					variant="text"
-					className={`BuySell_selector ${
-						buySellSelector === "sell" && "sell"
-					}`}
-					onClick={() => setBuySellSelector("sell")}
+					fullWidth
+					color="info"
+					variant="outlined"
+					onClick={handleSubmit}
 				>
-					Sell
+					Submit
 				</Button>
-			</div>
-			<label htmlFor="entryPrice">Entry price</label>
-			<input
-				type="text"
-				id="entryPrice"
-				name="entryPrice"
-				value={entryPrice}
-				onChange={(e) => setEntryPrice(e.target.value)}
-			/>
-			<label htmlFor="type">Type</label>
-			<select
-				name="type"
-				id="type"
-				value={type}
-				onChange={(e) => setType(e.target.value)}
-			>
-				<option value="market">Market</option>
-				<option value="limit">Limit</option>
-			</select>
-			<button type="submit" onClick={handleSubmit}>
-				Submit
-			</button>
-		</form>
+			</form>
+		</Container>
 	);
 }
 
